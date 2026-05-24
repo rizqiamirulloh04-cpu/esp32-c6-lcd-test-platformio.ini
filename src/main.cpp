75e06d80c303bc4405e12df5b151d8b132cfd93e
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-24 22:11:23 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,37 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 
-#define TFT_BL    15
-
 #define TFT_MOSI  6
 #define TFT_SCLK  7
 #define TFT_CS    14
-#define TFT_DC    8
-#define TFT_RST   9
+#define TFT_DC    15
+#define TFT_RST   21
+#define TFT_BL    22
 
-Adafruit_ST7789 tft = Adafruit_ST7789(
-  TFT_CS,
-  TFT_DC,
-  TFT_RST
-);
+SPIClass spi = SPIClass(FSPI);
+
+Adafruit_ST7789 tft = Adafruit_ST7789(&spi, TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
 
   Serial.begin(115200);
 
+  // Backlight
   pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
 
-  // coba HIGH dulu
-  digitalWrite(TFT_BL, LOW);
+  // SPI LCD
+  spi.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
 
-  SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
-
+  // Init LCD
   tft.init(172, 320);
 
   tft.setRotation(1);
 
-  tft.fillScreen(ST77XX_BLACK);
+  // Offset penting untuk 1.47"
+  tft.setAddrWindow(34, 0, 172, 320);
 
-  delay(500);
-
+  // Test warna
   tft.fillScreen(ST77XX_RED);
   delay(1000);
 
@@ -47,18 +45,18 @@ void setup() {
 
   tft.fillScreen(ST77XX_BLACK);
 
+  // Text
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(2);
 
   tft.setCursor(20, 40);
-  tft.println("HELLO");
+  tft.println("RC DASHBOARD");
 
-  tft.setCursor(20, 70);
+  tft.setCursor(20, 80);
   tft.println("ESP32-C6");
 
   tft.drawRect(5, 5, 310, 160, ST77XX_WHITE);
 }
 
 void loop() {
-
 }
