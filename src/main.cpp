f0137b9b5be41d9fa75eb8ca8618d10e3e535798
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-24 23:55:38 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
-#define TFT_BL   22
+#define TFT_BL   15
 
 #define TFT_MOSI 6
 #define TFT_SCLK 7
+#define TFT_DC   8
+#define TFT_RST  9
 #define TFT_CS   14
-#define TFT_DC   15
-#define TFT_RST  21
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
     TFT_DC,
@@ -20,14 +20,14 @@ Arduino_DataBus *bus = new Arduino_ESP32SPI(
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
     TFT_RST,
-    3,
-    true,
-    320,
-    172,
-    0,
-    34,
-    0,
-    34
+    1,      // rotation bawaan driver
+    true,   // IPS
+    172,    // width
+    320,    // height
+    34,     // col offset 1
+    0,      // row offset 1
+    34,     // col offset 2
+    0       // row offset 2
 );
 
 void setup()
@@ -37,22 +37,33 @@ void setup()
 
     gfx->begin();
 
-    gfx->invertDisplay(false);
+    // landscape
+    gfx->setRotation(1);
 
+    // background hitam
     gfx->fillScreen(0x0000);
 
+    // border putih
+    gfx->drawRect(0, 0, 320, 172, 0xFFFF);
+
+    // text hijau
     gfx->setTextColor(0x07E0);
     gfx->setTextSize(3);
 
-    gfx->setCursor(30, 40);
+    gfx->setCursor(20, 30);
     gfx->println("ESP32-C6");
 
-    gfx->setCursor(30, 90);
+    gfx->setCursor(20, 70);
     gfx->println("LANDSCAPE");
 
-    gfx->fillRect(20, 140, 80, 20, 0xF800);
-    gfx->fillRect(120, 140, 80, 20, 0x07E0);
-    gfx->fillRect(220, 140, 80, 20, 0x001F);
+    gfx->setCursor(20, 110);
+    gfx->println("OK");
+
+    // garis bawah merah
+    gfx->fillRect(20, 140, 120, 12, 0xF800);
+
+    // garis bawah hijau
+    gfx->fillRect(170, 140, 120, 12, 0x07E0);
 }
 
 void loop()
