6bb9dbb0390a943f0d88172dd4aef74d6571f29b
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-25 00:01:43 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
-#define TFT_BL   15
+#define TFT_BL    15
 
-#define TFT_MOSI 6
-#define TFT_SCLK 7
-#define TFT_DC   8
-#define TFT_RST  9
-#define TFT_CS   14
+#define TFT_MOSI  6
+#define TFT_SCLK  7
+#define TFT_DC    8
+#define TFT_RST   9
+#define TFT_CS    14
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
     TFT_DC,
@@ -20,14 +20,14 @@ Arduino_DataBus *bus = new Arduino_ESP32SPI(
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
     TFT_RST,
-    1,      // rotation bawaan driver
+    0,      // rotation default
     true,   // IPS
-    172,    // width
-    320,    // height
-    34,     // col offset 1
-    0,      // row offset 1
-    34,     // col offset 2
-    0       // row offset 2
+    172,
+    320,
+    34,
+    0,
+    34,
+    0
 );
 
 void setup()
@@ -35,35 +35,40 @@ void setup()
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH);
 
+    delay(200);
+
     gfx->begin();
+
+    delay(200);
 
     // landscape
     gfx->setRotation(1);
 
-    // background hitam
+    // test warna
+    gfx->fillScreen(0x0000);
+    delay(500);
+
+    gfx->fillScreen(0xF800);
+    delay(500);
+
+    gfx->fillScreen(0x07E0);
+    delay(500);
+
+    gfx->fillScreen(0x001F);
+    delay(500);
+
     gfx->fillScreen(0x0000);
 
-    // border putih
-    gfx->drawRect(0, 0, 320, 172, 0xFFFF);
-
-    // text hijau
-    gfx->setTextColor(0x07E0);
+    gfx->setTextColor(0xFFFF);
     gfx->setTextSize(3);
 
-    gfx->setCursor(20, 30);
+    gfx->setCursor(20, 40);
     gfx->println("ESP32-C6");
 
-    gfx->setCursor(20, 70);
-    gfx->println("LANDSCAPE");
+    gfx->setCursor(20, 90);
+    gfx->println("LCD OK");
 
-    gfx->setCursor(20, 110);
-    gfx->println("OK");
-
-    // garis bawah merah
-    gfx->fillRect(20, 140, 120, 12, 0xF800);
-
-    // garis bawah hijau
-    gfx->fillRect(170, 140, 120, 12, 0x07E0);
+    gfx->drawRect(0, 0, 320, 172, 0xFFFF);
 }
 
 void loop()
