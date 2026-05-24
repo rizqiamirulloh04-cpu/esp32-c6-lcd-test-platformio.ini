ee3531e364ed28be72fa0b46cdb17e612622b29f
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-24 23:45:25 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
+#define TFT_BL   22
+
 #define TFT_MOSI 6
 #define TFT_SCLK 7
 #define TFT_CS   14
 #define TFT_DC   15
 #define TFT_RST  21
-#define TFT_BL   22
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
     TFT_DC,
@@ -19,14 +20,14 @@ Arduino_DataBus *bus = new Arduino_ESP32SPI(
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
     TFT_RST,
-    0,
+    3,
     true,
-    172,
     320,
-    34,
+    172,
     0,
     34,
-    0
+    0,
+    34
 );
 
 void setup()
@@ -36,28 +37,22 @@ void setup()
 
     gfx->begin();
 
-    gfx->fillScreen(0xF800);
-    delay(1000);
-
-    gfx->fillScreen(0x07E0);
-    delay(1000);
-
-    gfx->fillScreen(0x001F);
-    delay(1000);
+    gfx->invertDisplay(false);
 
     gfx->fillScreen(0x0000);
 
-    gfx->setTextColor(0xFFFF);
-    gfx->setTextSize(2);
+    gfx->setTextColor(0x07E0);
+    gfx->setTextSize(3);
 
-    gfx->setCursor(20, 40);
-    gfx->println("WAVESHARE");
-
-    gfx->setCursor(20, 80);
+    gfx->setCursor(30, 40);
     gfx->println("ESP32-C6");
 
-    gfx->setCursor(20, 120);
-    gfx->println("LCD OK");
+    gfx->setCursor(30, 90);
+    gfx->println("LANDSCAPE");
+
+    gfx->fillRect(20, 140, 80, 20, 0xF800);
+    gfx->fillRect(120, 140, 80, 20, 0x07E0);
+    gfx->fillRect(220, 140, 80, 20, 0x001F);
 }
 
 void loop()
